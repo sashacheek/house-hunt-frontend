f6f0078e8bb297c44b80483bc5dcfa0aadcfa470
Sasha Cheek <sasha@cheek.tv>
2025-05-12 19:57:09 -0600

    Shorten github links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
             <p>HouseHunt is a mock site to view and create house listings. It is not a real site, and the houses listed are not real. HouseHunt is intended to demonstrate the developer's coding expertise.</p>
             <div>
                 <p>View the source code here:</p>
-                <p>Frontend: <a href="https://www.github.com/sashacheek/house-hunt-frontend" target="_blank">https://www.github.com/sashacheek/house-hunt-frontend</a></p>
-                <p>Backend: <a href="https://www.github.com/sashacheek/house-hunt-backend" target="_blank">https://www.github.com/sashacheek/house-hunt-backend</a></p>
+                <p>Frontend: <a href="https://www.github.com/sashacheek/house-hunt-frontend" target="_blank">github.com/sashacheek/house-hunt-frontend</a></p>
+                <p>Backend: <a href="https://www.github.com/sashacheek/house-hunt-backend" target="_blank">github.com/sashacheek/house-hunt-backend</a></p>
             </div>
         </footer>
     )
